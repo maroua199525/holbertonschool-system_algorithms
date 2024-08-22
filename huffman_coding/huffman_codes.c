c53a5f3c0c7418fa131d8b8576633e1d6403b4da
maroua199525 <2646@holbertonstudents.com>
2024-08-22 11:56:10 +0100

    fix task

--- huffman_coding/huffman_codes.c
+++ huffman_coding/huffman_codes.c
@@ -1,47 +1,67 @@
 #include "huffman.h"
 
-/**
- * print_huffman - Print Huffman tree
- * @node: Is a Pointer to root of tree
- * @bin: Is a Binary number
- *
- * Return: Nothing
- */
-void print_huffman(binary_tree_node_t *node, int bin)
-{
-	symbol_t *symbol = NULL;
-
-	if (!node)
-		return;
-	symbol = node->data;
-	if (symbol->data != -1)
-		printf("%c: %s\n", symbol->data, itoa(bin, 2) + 1);
-	else
-	{
-			print_huffman(node->left, bin << 1);
-			print_huffman(node->right, (bin << 1) + 1);
-	}
-	free(symbol);
-	free(node);
-}
+void print_huffman_codes(binary_tree_node_t *tree, unsigned long path);
 
 /**
- * huffman_codes - Build the Huffman tree and print the resulting Huffman
- * codes for each symbol
- * @data: Is an array of characters of size size
- * @freq: Is an array containing the associated frequencies (of size size too)
- * @size: Size of data
- *
- * Return: 1 if it succeed, 0 if it fails
+ * huffman_codes - generates character codes from huffman tree
+ * @data: array of characters
+ * @freq: frequency of each character
+ * @size: size of data and freq arrays
+ * Return: 1 on success else 0
  */
 int huffman_codes(char *data, size_t *freq, size_t size)
 {
-	binary_tree_node_t *node = NULL;
+	binary_tree_node_t *tree = huffman_tree(data, freq, size);
 
-	if (!data || !freq || !size)
+	if (!tree)
 		return (0);
-
-	node = huffman_tree(data, freq, size);
-	print_huffman(node, 1);
+	print_huffman_codes(tree, 1);
 	return (1);
+}
+
+/**
+ * print_huffman_codes - walks the tree to print the huffman codes
+ * @tree: pointer to current tree node
+ * @path: long int holding binary representation of path
+ */
+void print_huffman_codes(binary_tree_node_t *tree, unsigned long path)
+{
+	symbol_t *symbol;
+
+	if (!tree)
+		return;
+	symbol = tree->data;
+	if (symbol->data != -1)
+	{
+		printf("%c: %s\n", symbol->data, convert(path, 2) + 1);
+	}
+	else
+	{
+		print_huffman_codes(tree->left, path << 1);
+		print_huffman_codes(tree->right, (path << 1) + 1);
+	}
+	free(symbol);
+	free(tree);
+}
+/**
+ * convert - converts a number to a custom base
+ * @num: the number to convert
+ * @base: the desired base
+ * Return: pointer to string containing digits in new base
+ */
+char *convert(long num, long base)
+{
+	char *DIGITS = "0123456789ABCDEFG", *ptr;
+	static char buf[66];
+	short neg = num < 0 ? 1 : 0;
+
+	ptr = &buf[sizeof(buf)];
+	*--ptr = 0;
+	do {
+		*--ptr = DIGITS[ABS(num % base)];
+		num /= base;
+	} while (num);
+	if (neg)
+		*--ptr = '-';
+	return (ptr);
 }
